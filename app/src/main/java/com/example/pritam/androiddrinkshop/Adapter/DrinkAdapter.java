ebfca657f8251412f7bd46642f36904409c146c6
pritam786 <pritammaiti11@gmail.com>
2018-08-30 17:54:51 +0530

    Add To Cart

--- app/src/main/java/com/example/pritam/androiddrinkshop/Adapter/DrinkAdapter.java
+++ app/src/main/java/com/example/pritam/androiddrinkshop/Adapter/DrinkAdapter.java
@@ -1,13 +1,20 @@
 package com.example.pritam.androiddrinkshop.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.pritam.androiddrinkshop.Interface.ItemClickListener;
 import com.example.pritam.androiddrinkshop.Model.Drink;
 import com.example.pritam.androiddrinkshop.R;
@@ -33,10 +40,17 @@ public class DrinkAdapter extends RecyclerView.Adapter<DrinkViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DrinkViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull DrinkViewHolder holder, final int position) {
 
         holder.txt_price.setText(new StringBuilder("$").append(drinkList.get(position).Price.toString()));
         holder.txt_drink_name.setText(drinkList.get(position).Name);
+
+        holder.btn_add_to_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddToCartDialog(position);
+            }
+        });
 
         Picasso.with(context)
                 .load(drinkList.get(position).Link)
@@ -48,6 +62,54 @@ public class DrinkAdapter extends RecyclerView.Adapter<DrinkViewHolder> {
                 Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    private void showAddToCartDialog(int position) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        View itemView = LayoutInflater.from(context)
+                .inflate(R.layout.add_to_cart_layout,null);
+
+        //View
+        ImageView img_product_dialog = (ImageView)itemView.findViewById(R.id.img_cart_product);
+        ElegantNumberButton txt_count = (ElegantNumberButton)itemView.findViewById(R.id.txt_count);
+        TextView txt_product_dialog = (TextView)itemView.findViewById(R.id.txt_cart_product_name);
+
+        EditText edt_comment = (EditText)itemView.findViewById(R.id.edt_comment);
+
+        RadioButton rdi_sizeM = (RadioButton)itemView.findViewById(R.id.rdi_sizeM);
+        RadioButton rdi_sizeL = (RadioButton)itemView.findViewById(R.id.rdi_sizeL);
+
+        RadioButton rdi_sugar_100 = (RadioButton)itemView.findViewById(R.id.rdi_sugar_100);
+        RadioButton rdi_sugar_70 = (RadioButton)itemView.findViewById(R.id.rdi_sugar_70);
+        RadioButton rdi_sugar_50 = (RadioButton)itemView.findViewById(R.id.rdi_sugar_50);
+        RadioButton rdi_sugar_30 = (RadioButton)itemView.findViewById(R.id.rdi_sugar_30);
+        RadioButton rdi_sugar_free = (RadioButton)itemView.findViewById(R.id.rdi_sugar_free);
+
+        RadioButton rdi_ice_100 = (RadioButton)itemView.findViewById(R.id.rdi_ice_100);
+        RadioButton rdi_ice_70 = (RadioButton)itemView.findViewById(R.id.rdi_ice_70);
+        RadioButton rdi_ice_50 = (RadioButton)itemView.findViewById(R.id.rdi_ice_50);
+        RadioButton rdi_ice_30 = (RadioButton)itemView.findViewById(R.id.rdi_ice_30);
+        RadioButton rdi_ice_free = (RadioButton)itemView.findViewById(R.id.rdi_ice_free);
+
+        //Set Data
+        Picasso.with(context)
+                .load(drinkList.get(position).Link)
+                .into(img_product_dialog);
+
+        txt_product_dialog.setText(drinkList.get(position).Name);
+
+        builder.setView(itemView);
+        builder.setNegativeButton("ADD TO CART", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+
 
     }
 
